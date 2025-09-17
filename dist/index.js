2f012eaaeea4822d10119ddf84f31450df0206a1
aldvincent <aldvincent@microsoft.com>
2025-09-17 22:33:04 +0530

    remove dist

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Main entry point - delegates to unified CLI
-const cli_1 = require("./cli");
-// Parse command line arguments and run
-cli_1.program.parse();
-//# sourceMappingURL=index.js.map
